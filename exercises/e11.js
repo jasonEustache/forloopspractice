608564d707f7fe6925ffcf14596d741b12a66bd7
Andrey Codez <andreycodez@bixtrix.com>
2023-02-10 16:00:17 +0500

    Update e11 to add description of empty withdrawals in the task

--- exercises/e11.js
+++ exercises/e11.js
@@ -1,7 +1,8 @@
 // EXERCISE 11
-// Return an array of withdrawal sums of each bank account
+// Return an array of withdrawal sums of each bank account.
+// If the account doesn't have withdrawals, it's sum is 0.
 // Array example: bankAccounts in /data/data.js
-// getAllWithdrawals(bankAccounts) => [3432, 43242.34, 23432]
+// getAllWithdrawals(bankAccounts) => [3432, 0, 43242.34, 0, 23432]
 
 export function getAllWithdrawals(array) {
   // Your code goes here...
